d6af8a63d35593cd560293e6985c552c203ff722
Alexander Basov <omnecrone@ya.ru>
2018-12-07 18:02:57 +0300

    Initial Commit

---
+++ TestWorkMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestWorkMovies
+//
+//  Created by AnnBas on 07/12/2018.
+//  Copyright © 2018 BAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
